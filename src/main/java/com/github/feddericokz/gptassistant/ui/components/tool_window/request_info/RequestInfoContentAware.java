9fe81967e2fcb980d585d499e9652832d8dd6999
Federico von Wernich <feddericokz @gmail.com>
2024-05-13 22:03:05 -0300

    Fix verification.

--- src/main/java/com/github/feddericokz/gptassistant/ui/components/tool_window/request_info/RequestInfoContentAware.java
+++ src/main/java/com/github/feddericokz/gptassistant/ui/components/tool_window/request_info/RequestInfoContentAware.java
@@ -22,7 +22,12 @@ public interface RequestInfoContentAware {
      * Method com.github.feddericokz.gptassistant.ui.components.tool_window.request_info.RequestInfoContentAware.updateToolWindowContent(String update) : void contains an *invokeinterface* instruction referencing a private method com.github.feddericokz.gptassistant.ui.components.tool_window.request_info.RequestInfoContentAware.lambda$updateToolWindowContent$0(ToolWindowContent content, String update) : void. This can lead to **IncompatibleClassChangeError** exception at runtime.
      */
     default Runnable internalUpdateContent(ToolWindowContent content, String update) {
-        return () -> updateContent(content, update);
+        return new Runnable() {
+            @Override
+            public void run() {
+                updateContent(content, update);
+            }
+        };
     }
 
 }
